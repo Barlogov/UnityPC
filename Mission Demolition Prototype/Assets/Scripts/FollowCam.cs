eb5ed0245278400d8c903c2110bb8c8902398502
Barlogov <43818846+Barlogov@users.noreply.github.com>
2020-03-07 21:48:55 +0300

    Mission Demolition Prototype
    Add Mission Demolition Prototype and fix APP

---
+++ Mission Demolition Prototype/Assets/Scripts/FollowCam.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour {
+
+	static public GameObject POI; // Интересующий объект point of interest
+	[Header("Set in Inspector")]
+	public float easing = 0.05f;
+	public Vector2 minXY = Vector2.zero;
+
+	[Header("Set Dynamically")]
+	public float camZ; // Желаемая координата Z для камеры
+
+	void Awake(){
+		camZ = this.transform.position.z;
+	}
+
+	private void FixedUpdate() {
+		if (POI == null) return;
+
+		// Получить позицию интересующего объекта
+		Vector3 destination = POI.transform.position;
+		// Ограничить X и Y минимальными значениями ( чтобы не видить что под землёй)
+		destination.x = Mathf.Max(minXY.x, destination.x);
+		destination.y = Mathf.Max(minXY.y, destination.y);
+		// Определить точку между текущим местоположением камеры и destination
+		destination = Vector3.Lerp(transform.position, destination, easing);
+		// Принудительно установить значение destination.z равным camZ, чтобы отодвинуть камеру подальше
+		destination.z = camZ;
+		// Поместить камеру в позицию destination
+		transform.position = destination;	
+		// Изменить размер обозримого(orthographicSize) камеры, чтобы земля оставалась в поле зрения всегда
+		Camera.main.orthographicSize = destination.y+10;
+
+	}
+}
---
+++ Mission Demolition Prototype/Assets/Scripts/Slingshot.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour {
+
+	[Header("Set in Inspector:")]
+	public GameObject prefubProjectile;
+	public float velocityMult = 8f;
+
+	[Header("Set Dynamically")]
+	public GameObject launchPoint;
+	public Vector3 launchPos;
+	public GameObject projectile;
+	public bool aimingMode;
+	
+	private Rigidbody projectileRigidbody;
+
+
+	void Awake() {
+		Transform launchPointTrans = transform.Find("LounchPoint");  //Поиск дочернего объекта и возврат компонента Transform
+		launchPoint = launchPointTrans.gameObject;                   //Получение игрового объекта, владеющего этим Transform
+		launchPoint.SetActive(false);
+		launchPos = launchPointTrans.position;
+	}
+
+	// Введение и выход мышки из колайдера объекта
+	void OnMouseEnter() {
+		//print("Slingshot:OnMouseEnter()");
+		launchPoint.SetActive(true);
+	}
+
+	void OnMouseExit() {
+		//print("Slingshot:OnMouseExit()");
+		launchPoint.SetActive(false);
+	}
+
+	private void OnMouseDown() {
+		aimingMode = true;
+		projectile = Instantiate(prefubProjectile) as GameObject;
+		projectile.transform.position = launchPos;
+		//projectile.GetComponent<Rigidbody>().isKinematic = true; // Делет объект кинематическим
+		projectileRigidbody = projectile.GetComponent<Rigidbody>();
+		projectileRigidbody.isKinematic = true;
+	}
+
+	private void Update() {
+		if(!aimingMode) return;
+			// Получение координат мыши
+			Vector3 mousePos2D = Input.mousePosition;
+			mousePos2D.z = -Camera.main.transform.position.z;
+			Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+
+			// 
+			Vector3 mouseDelta = mousePos3D - launchPos;
+			// Ограничить дельту радиусом колайдера рогадки
+			float maxMagnitude = this.GetComponent<SphereCollider>().radius;
+			if(mouseDelta.magnitude > maxMagnitude){
+				mouseDelta.Normalize();
+				mouseDelta *= maxMagnitude;	
+			}
+
+			// Передвинуть снаряд
+			Vector3 projPos = launchPos + mouseDelta;
+			projectile.transform.position = projPos;
+			if(Input.GetMouseButtonUp(0)){
+				aimingMode = false;
+				projectileRigidbody.isKinematic = false;
+				projectileRigidbody.velocity = -mouseDelta*velocityMult;
+				FollowCam.POI = projectile;
+				projectile=null;
+			}
+
+
+	}
+}
